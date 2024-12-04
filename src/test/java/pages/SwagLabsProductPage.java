5867033b624bc69ecfc4a85f2adcba36236c7416
Jhesua <jhesua_machado@hotmail.com>
2024-12-03 22:05:29 -0300

    Fazendo outro page object

--- src/test/java/pages/SwagLabsProductPage.java
+++ src/test/java/pages/SwagLabsProductPage.java
@@ -1,6 +1,19 @@
 package pages;
 
+import org.openqa.selenium.By;
+
 public class SwagLabsProductPage
 {
-
+    private String URL;
+    private By carrinho;
+    private By filtro;
+        private By fNameAtoZ;
+        private By fNameZtoA;
+        private By fPriceLowToHigh;
+        private By fPriceHightoLow;
+    private By leftBar;
+        private By lbAllItens;
+        private By lbAbout;
+        private By lbLogout;
+        private By lbResetAppState;
 }
